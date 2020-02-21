8745f5a83136b93fc5b6dc66f2a85fa44b6523a3
pingt9*t <pressntr@gmail.com>
2020-02-20 22:20:01 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Nate Storrs React Portfolio Application
+
+### After downloading
+- $ npm install
+- $ npm run start
+
+### To Edit The App
+- This might change as I build the app
+- Go into src/components/app.js
+- Add whatever jsx you want to render
---
+++ src/components/portfolio-container.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class PortfolioContainer extends Component {
+    render() {
+        return (
+            <div className="port-contianer">
+                <h1>Port Items go here...</h1>
+            </div>
+        )
+    }
+}
