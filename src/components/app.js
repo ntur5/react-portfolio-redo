571b2e638f969e0efeb6e25ed57a8fb4b0976aba
pingt9*t <pressntr@gmail.com>
2020-02-24 15:23:13 -0700

    Added a ternary for the blog button

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
-import PortfolioContainer from './portfolio-container';
+import PortfolioContainer from './portfolio/portfolio-container';
+import NavigationContainer from './navigation/naviation-container'
 import moment from 'moment';
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
+        <NavigationContainer />
         <h1>NaTHaN StoRRs Portfolio</h1>
         <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
         <PortfolioContainer />
