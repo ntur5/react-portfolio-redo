544c64b85bec177324c1f5d25a41a4a1dc53aac8
pingt9*t <pressntr@gmail.com>
2020-02-27 21:12:35 -0700

    Creating an Authentication Session in React

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class Login extends Component {
     constructor(props) {
@@ -20,16 +21,24 @@ export default class Login extends Component {
     }
 
     handleSubmit(event) {
-        console.log("Handle Submit", event)
+        axios.post("https://api.devcamp.space/sessions",
+        {
+            client: {
+                email: this.state.email,
+                password: this.state.password
+            }
+        },
+        { withCredentials: true }
+        ).then(response => {
+            console.log("response", response)
+        })
+        event.preventDefault()
     }
     
     render() {
         return (
             <div>
                 <h1>LOGIN TO ACCESS YOUR DASHBOARD</h1>
-
-                <h2>{this.state.email}</h2>
-                <h2>{this.state.password}</h2>
 
                 <form onSubmit={this.handleSubmit}>
                     <input 
