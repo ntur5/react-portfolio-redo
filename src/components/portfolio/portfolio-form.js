fd2c5aa510b4d9ca7e44f052dd5535600262d864
pingt9*t <pressntr@gmail.com>
2020-03-02 16:13:42 -0700

    Integrating React Dropzone Component into the Portfolio Form

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -24,11 +24,18 @@ export default class PortfolioForm extends Component {
         this.handleSubmit = this.handleSubmit.bind(this)
         this.componentConfig = this.componentConfig.bind(this)
         this.djsConfig = this.djsConfig.bind(this)
+        this.handleThumbDrop = this.handleThumbDrop.bind(this)
+    }
+
+    handleThumbDrop() {
+        return {
+            addedfile: file => this.setState({ thumb_image: file})
+        }
     }
 
     componentConfig() {
         return {
-            iconFiletypes: [".jpg", ".png", ".joop"],
+            iconFiletypes: [".jpg", ".png"],
             showFiletypeIcon: true,
             postUrl: "https://httpbin.org/post"            
         }
@@ -52,6 +59,9 @@ export default class PortfolioForm extends Component {
             formData.append("portfolio_item[url]", this.state.url)
             formData.append("portfolio_item[category]", this.state.category)
             formData.append("portfolio_item[position]", this.state.position)
+            if (this.state.thumb_image) {
+                formData.append("portfolio_item[thumb_image]", this.state.thumb_image)
+            }
 
             return formData;
     }
@@ -133,6 +143,7 @@ export default class PortfolioForm extends Component {
                         <DropzoneComponent 
                             config={this.componentConfig()}
                             djsConfig={this.djsConfig()}
+                            eventHandlers={this.handleThumbDrop()}
                         >
                         </DropzoneComponent>
                     </div>
