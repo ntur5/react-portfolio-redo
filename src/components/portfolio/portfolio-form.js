92523ac604aab7c60822510fee544d9d934b7d45
pingt9*t <pressntr@gmail.com>
2020-03-03 10:56:45 -0700

    Building CSS Grid Styles for the Portfolio Form

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -131,7 +131,7 @@ export default class PortfolioForm extends Component {
     render() {
         return (
                 <form className="portfolio-form-wrapper" onSubmit={this.handleSubmit}>
-                    <div>
+                    <div className="two-column">
                         <input 
                             type="text"
                             name="name"
@@ -147,7 +147,7 @@ export default class PortfolioForm extends Component {
                             onChange={this.handleChange}
                         />
                     </div>
-                    <div>
+                    <div className="two-column">
                         <input 
                             type="text"
                             name="position"
@@ -167,7 +167,7 @@ export default class PortfolioForm extends Component {
                         </select>
                     </div>
 
-                    <div>
+                    <div className="one-column">
                         <textarea 
                             type="text"
                             name="description"
@@ -176,7 +176,7 @@ export default class PortfolioForm extends Component {
                             onChange={this.handleChange}
                         />
                     </div>
-                    <div className="image-uploaders">
+                    <div className="image-uploaders three-column">
                         <DropzoneComponent 
                             ref={this.thumbRef}
                             config={this.componentConfig()}
