af0dc0914151b5dabd0649b4271a2729cf1d7c07
pingt9*t <pressntr@gmail.com>
2020-03-03 11:58:00 -0700

    Updating the Dropzone Labels with Child Components to Finalize the Form

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -176,13 +176,14 @@ export default class PortfolioForm extends Component {
                             onChange={this.handleChange}
                         />
                     </div>
-                    <div className="image-uploaders three-column">
+                    <div className="image-uploaders">
                         <DropzoneComponent 
                             ref={this.thumbRef}
                             config={this.componentConfig()}
                             djsConfig={this.djsConfig()}
                             eventHandlers={this.handleThumbDrop()}
                         >
+                            <div className="dz-message">Thumbnail</div>
                         </DropzoneComponent>
                         <DropzoneComponent 
                             ref={this.bannerRef}
@@ -190,6 +191,7 @@ export default class PortfolioForm extends Component {
                             djsConfig={this.djsConfig()}
                             eventHandlers={this.handleBannerDrop()}
                         >
+                        <div className="dz-message">Banner</div>
                         </DropzoneComponent>
                         <DropzoneComponent 
                             ref={this.logoRef}
@@ -197,10 +199,11 @@ export default class PortfolioForm extends Component {
                             djsConfig={this.djsConfig()}
                             eventHandlers={this.handleLogoDrop()}
                         >
+                        <div className="dz-message">Logo</div>
                         </DropzoneComponent>
                     </div>
                     <div>
-                        <button type="submit">Save</button>
+                        <button className="btn" type="submit">Save</button>
                     </div>
                 </form>
         )
