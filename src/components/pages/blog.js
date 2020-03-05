c8e3783868493a603b978b178c10b7c331c0a270
pingt9*t <pressntr@gmail.com>
2020-03-05 16:15:58 -0700

    Converting the Functional Blog Component into a Class Component, Calling the Blog API and Storing the Data in State

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 export default class Blog extends Component {
     constructor() {
         super()
+        this.state = {
+            blogItems: []
+        }
+
+        this.getBlogItems = this.getBlogItems.bind(this)
     }
+
+    getBlogItems() {
+        axios.get('https://nathanstorrrs.devcamp.space/portfolio/portfolio_blogs', { withCredentials: true })
+        .then(response => {
+            this.setState({
+                blogItems: response.data.portfolio_blogs
+            })
+        })
+        .catch(error => {
+            console.log('getBlogItems', error)
+        })
+    }
+
+    componentDidMount() {
+        this.getBlogItems() 
+
+    }
+
     render() {
-    return (
-        <div>
-            <h2>Blog</h2>
+        return (
             <div>
-                <Link to="/about-me">Read more about myself</Link>
+                <h2>Blog</h2>
+                <div>
+                    <Link to="/about-me">Read more about myself</Link>
+                </div>
             </div>
-        </div>
-    )
+        )
     }
 }
