b1de3b85b2a697e9fdc839d84a44d48958597b8a
pingt9*t <pressntr@gmail.com>
2020-03-06 13:48:58 -0700

    Getting the Count of Total Records on the Server and Updating the Component State

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -7,7 +7,9 @@ export default class Blog extends Component {
     constructor() {
         super()
         this.state = {
-            blogItems: []
+            blogItems: [],
+            totalCount: 0,
+            currentPage: 0
         }
 
         this.getBlogItems = this.getBlogItems.bind(this);
@@ -27,10 +29,14 @@ export default class Blog extends Component {
     }
 
     getBlogItems() {
+        this.setState({
+            currentPage: this.state.currentPage + 1
+        })
         axios.get('https://nathanstorrrs.devcamp.space/portfolio/portfolio_blogs', { withCredentials: true })
         .then(response => {
             this.setState({
-                blogItems: response.data.portfolio_blogs
+                blogItems: response.data.portfolio_blogs,
+                totalCount: response.data.meta.total_records
             })
         })
         .catch(error => {
